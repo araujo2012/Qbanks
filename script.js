4ccb01b3b943ed846a0c5bb2baa1538837fc6110
lucas <araujo2012@hotmail.com>
2021-09-08 18:42:48 -0300

    principal finalizado

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import { alou } from './exercises.js';
-
 // adiciona um novo elemento com um texto e class especifico;
 function newChild(type, text, classs) {
     let newChildItem = document.createElement(type);
@@ -10,12 +8,25 @@ function newChild(type, text, classs) {
     return newChildItem;
 }
 
-console.log(exercises);
-alou();
-// cria um titulo na pagina
+function selectExercicio(valor) {
+    let sum = 0;
+    for(value of valor) {
+        sum += value;
+    }
+    let aux = Math.floor(Math.random()*sum);
+    for(let index = 0; index < valor.length; index += 1) {
+        aux -= valor[index];
+        if (aux < 0){
+            return index;
+        }
+    }
+    return 'problem';
+}
 
-//Adicione a classe description nas 3 tags h3 criadas;
-//Remova a section criado no passo 5 (aquele que possui a classe left-content ). Utilize a função .removeChild() ;
-//Centralize a section criado no passo 6 (aquele que possui a classe right-content ). Dica: para centralizar, basta configurar o margin-right: auto da section ;
-//Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content ) para a cor verde;
-//Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
+let exercise = selectExercicio(valor);
+console.log(exercise);
+
+let body = document.querySelector('body');
+body.append(newChild('h1', titulos[exercise], 'title'));
+body.append(newChild('p', questions[exercise], 'title'));
+body.append(newChild('p', respostas[exercise], 'title'));
---
+++ setupExercises.js
@@ -0,0 +1,38 @@
+let tags = [];
+let times = [];
+let titulos = [];
+let questions = [];
+let respostas = [];
+let valor = [];
+
+//exercicio 0
+tags.push(['cubo-magico', 'speedrun', 'reforco']);
+times.push(60);
+titulos.push('Speedrunner de cubo-magico');
+questions.push('Resolva o cubo magico em menos de 30 segundos.');
+respostas.push('Nao tem');
+valor.push(1);
+
+//exercicio 1
+tags.push(['calculo', 'derivada', 'reforco']);
+times.push(60);
+titulos.push('Resolvendo derivadas');
+questions.push('resolva algumas derivadas do seguinte link: https://www.math-exercises.com/limits-derivatives-integrals/derivative-of-a-function');
+respostas.push('https://www.math-exercises.com/answers-derivative-of-a-function');
+valor.push(1);
+
+//exercicio 2
+tags.push(['programacao', 'reforco']);
+times.push(60);
+titulos.push('Programando');
+questions.push('resolva dois exercicios do https://www.codewars.com/dashboard');
+respostas.push('nao tem');
+valor.push(5);
+
+//exercicio 3
+tags.push(['programacao', 'desafio']);
+times.push(60);
+titulos.push('Programando');
+questions.push('resolva o ultimo codeforces completo no link https://codeforces.com/');
+respostas.push('nao tem');
+valor.push(1);
